f403551877a51d1618be4004112a0dd21abde555
JamieDawson <jamierobertdawson@gmail.com>
2019-08-23 19:29:31 -0700

    updated dog api js. Need to handle sub breed

--- playground.awesome_dog/code/dog_search.js
+++ playground.awesome_dog/code/dog_search.js
@@ -1,9 +1,32 @@
 var http = require('http')
 var console = require('console')
 
-module.exports.function = function dog_search (breed) {
+module.exports.function = function dog_search (breed, subbreed) {
   var results = []
-  var dogAPI = "https://dog.ceo/api/breeds/image/random"
+
+  //two
+  //var dogAPI_two = "https://dog.ceo/api/breed/retriever/golden/images/random"
+
+  //one
+  //var dogAPI_one = "https://dog.ceo/api/breed/boxer/images/random"
+
+  if (subbreed)
+  {
+    console.log(tessst)
+  }
+
+  if (!subbreed){
+    if (breed) {
+      var dogAPI = "https://dog.ceo/api/breed/"
+      dogAPI = dogAPI.concat(breed)
+      dogAPI = dogAPI.concat("/images/random")
+      
+      console.log(dogAPI)
+      console.log(breed)
+    }
+  }
+
+
   var template = {}
 
   var tmpResults = http.getUrl(dogAPI, {format: 'text'});
